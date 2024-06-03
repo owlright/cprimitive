6b15c434ad9903e127c8159e3c1b906ce7ecdd4f
owlright <71282734+owlright@users.noreply.github.com>
2024-06-03 16:07:48 +0800

    refactor: DestroyRasterizedLines only considers RasterizedLines

--- rasterizer.c
+++ rasterizer.c
@@ -1,13 +1,9 @@
 #include <stdlib.h>
 #include "rasterizer.h"
 
-void DestroyRasterizedLines(void* obj)
+void DestroyRasterizedLines(RasterizedLines* lines)
 {
-    // TODO free lines
-    RasterizedLines* _lines = (RasterizedLines*)obj;
-    for (int i = 0; i < _lines->h; i++) {
-        // for (int j = 0; j < _lines->w; j++) {
-            free(_lines->lines[i]);
-        // }
+    for (int i = 0; i < lines->h; i++) {
+        free(lines->lines[i]);
     }
 }
