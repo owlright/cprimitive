8f98c5b55bce5ef98296abc51b03b3347b59eb20
owlright <71282734+owlright@users.noreply.github.com>
2024-07-03 17:02:13 +0800

    feat(RasterLine): The algorithm is now complete and can draw lines at any angle

--- line.c
+++ line.c
@@ -10,6 +10,65 @@ void swap(int* a, int* b)
     *b = temp;
 }
 
+// ! 以下实现将grid的左上角坐标视作grid自身坐标
+// plow line slope between -1 and 1
+static void RasterizeLineLow(int x0, int y0, int x1, int y1, Scanline* lines)
+{
+    int dx = x1 - x0;
+    int dy = y1 - y0;
+    int yi = 1;
+    if (dy < 0) {
+        yi = -1;
+        dy = -dy;
+    }
+    int D = 2 * dy - dx;
+    int y = y0;
+    int line_number = 0;
+    for (int x = x0; x <= x1; x++) {
+        line_number = abs(y - y0);
+        if (lines[line_number].x1 == -1) { // the first point in the line
+            lines[line_number].x1 = x;
+            lines[line_number].y = y;
+        }
+        lines[line_number].x2 = x; // update x2 anyway
+        if (D > 0) {
+            y = y + yi;
+            D = D + 2 * dy - 2 * dx;
+        } else {
+            D = D + 2 * dy;
+        }
+    }
+}
+
+// high line slope greater than 1 or less than -1
+static void RasterizeLineHigh(int x0, int y0, int x1, int y1, Scanline* lines)
+{
+    int dx = x1 - x0;
+    int dy = y1 - y0;
+    int xi = 1;
+    if (dx < 0) {
+        xi = -1;
+        dx = -dx;
+    }
+    int D = 2 * dx - dy;
+    int x = x0;
+    int line_number = 0;
+    for (int y = y0; y <= y1; y++) {
+        line_number = y - y0 > 0 ? y - y0 : y0 - y; // maybe abs() is ok
+        if (lines[line_number].x1 == -1) { // the first point in the line
+            lines[line_number].x1 = x;
+            lines[line_number].y = y;
+        }
+        lines[line_number].x2 = x; // update x2 anyway
+        if (D > 0) {
+            x = x + xi;
+            D = D + 2 * dx - 2 * dy;
+        } else {
+            D = D + 2 * dx;
+        }
+    }
+}
+
 static RasterizedLines RasterizeLine(Shape* obj)
 {
     // log_info_no_args();
@@ -18,21 +77,16 @@ static RasterizedLines RasterizeLine(Shape* obj)
     int Y0 = t->p1.y;
     int X1 = t->p2.x;
     int Y1 = t->p2.y;
-    // Make sure X0 <= X1
-    if (X0 > X1) {
-        swap(&X0, &X1);
-        swap(&Y0, &Y1);
-    }
+
     int dx = X1 - X0;
     int dy = Y1 - Y0;
-    int dy_abs = abs(dy);
     log_debug("dx: %d, dy: %d", dx, dy);
 
-    Scanline* lines = calloc(dy_abs + 1, sizeof(Scanline));
-    for (int i = 0; i <= dy_abs; i++) {
+    Scanline* lines = calloc(abs(dy) + 1, sizeof(Scanline));
+    for (int i = 0; i <= abs(dy); i++) {
         lines[i].x1 = -1;
     }
-
+    RasterizedLines result;
     // ! Special case for vertical lines and horizontal lines
     if (dx == 0) {
         for (int j = 0; j <= dy; j++) {
@@ -40,66 +94,46 @@ static RasterizedLines RasterizeLine(Shape* obj)
             lines[j].x1 = X0;
             lines[j].x2 = X0;
         }
-        RasterizedLines result = { lines, dy_abs + 1 };
+        result.lines = lines;
+        result.h = abs(dy) + 1;
         return result;
     }
     if (dy == 0) {
         lines[0].y = Y0;
         lines[0].x1 = X0;
         lines[0].x2 = X1;
-        RasterizedLines result = { lines, 1 };
+        result.lines = lines;
+        result.h = 1;
         return result;
     }
-    if (dx == dy_abs) {
+    if (dx == abs(dy)) {
         for (int i = 0; i <= dx; i++) {
             int j = dy > 0 ? i : -i;
             lines[i].y = Y0 + j;
             lines[i].x1 = X0 + i;
             lines[i].x2 = X0 + i;
         }
-        RasterizedLines result = { lines, dy_abs + 1 };
+        result.lines = lines;
+        result.h = abs(dy) + 1;
         return result;
     }
-    // Digital Differential Analyzer (DDA)
-    // int steps = dx > dy_abs ? dx : dy_abs;
-    // double slope = dy / (double)dx; // slope can be < 0
 
-    // double x0 = X0 + 0.5;
-    // double y0 = Y0 + 0.5;
-    // double x1 = X1 + 0.5;
-    // double delta_x = dx / (double)steps;
-    // double x = x0;
-    // double y = y0;
-    // while (x <= x1) {
-    //     double _i = x - x0;
-    //     double _j = dy > 0 ? y - y0 : y0 - y;
-    //     int i = round(_i);
-    //     int j = round(_j);
-    //     if (lines[j].x1 == -1) { // the first point in the line
-    //         lines[j].y = dy > 0 ? j + y0 : y0 - j;
-    //         lines[j].x1 = i + x0;
-    //     }
-    //     lines[j].x2 = i + x0;
-    //     x += delta_x;
-    //     y = y0 + slope * (x - x0); // TODO: add slope*step each time is ok
-    // }
     // Bresenham's line algorithm
-    int D = 2 * dy - dx;
-    int y = Y0;
-    for (int x = X0; x <= X1; x++) {
-        int j = dy > 0 ? y - Y0 : Y0 - y;
-        if (lines[j].x1 == -1) { // the first point in the line
-            lines[j].y = dy > 0 ? y : Y0 - j;
-            lines[j].x1 = x;
+    if (abs(dy) < abs(dx)) { // |slope| < 1
+        if (X0 > X1) {
+            RasterizeLineLow(X1, Y1, X0, Y0, lines);
+        } else {
+            RasterizeLineLow(X0, Y0, X1, Y1, lines);
         }
-        lines[j].x2 = x;
-        if (D > 0) {
-            y = y + 1;
-            D = D - 2 * dx;
+    } else {
+        if (Y0 > Y1) {
+            RasterizeLineHigh(X1, Y1, X0, Y0, lines);
+        } else {
+            RasterizeLineHigh(X0, Y0, X1, Y1, lines);
         }
-        D = D + 2 * dy;
     }
-    RasterizedLines result = { lines, dy_abs + 1 };
+    result.lines = lines;
+    result.h = abs(dy) + 1;
     return result;
 }
 
